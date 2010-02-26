23d3e20a2dccb5065af41f1232d66f1148f8e6b8
Kirit Sælensminde <k@kirit.com>
2010-02-26 16:35:40 +0000

    The Windows scripts now include error handling.

--- test-fost.cmd
+++ test-fost.cmd
@@ -1,9 +1,19 @@
 @echo off
-for %%t in (fost-base fost-internet fost-orm fost-postgres fost-py fost-meta) DO (
+for %%t in (fost-base fost-internet fost-aws fost-orm fost-postgres fost-py fost-meta) DO (
     for %%b in (37 38 39 40 41 42) DO (
         call do-build.cmd %%t %%b %%t
-        for %%s in (-rc) DO (
-            call do-build.cmd %%t%%s %%b %%t
+        IF ERRORLEVEL 1 (
+            copy
+            GOTO end
+        ) ELSE (
+            for %%s in (-rc) DO (
+                call do-build.cmd %%t%%s %%b %%t
+                IF ERRORLEVEL 1 (
+                    copy
+                    GOTO end
+                )
+            )
         )
     )
 )
+:end
