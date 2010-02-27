a16f067c2bdb84f2fceb862b8d61f552a6d6d825
Kirit Sælensminde <k@kirit.com>
2010-02-27 12:28:22 +0000

    Now it tries to replicate what the existing bash script does for Boost versions.

--- test-fost.py
+++ test-fost.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os
+import os, sys
 
 
 PROJECTS = {
@@ -16,8 +16,15 @@ PROJECTS = {
 }
 SUFFIXES = ['', '-rc']
 BOOST_VERSIONS = [[b, 0] for b in range(38, 43)]
-BOOST_VERSIONS.append(['karmic'])
+if sys.platform == 'linux2':
+    BOOST_VERSIONS.append(['karmic'])
 
+
+def is_windows():
+    """
+        Returns True if the system is Windows.
+    """
+    return sys.platform == 'win32'
 
 def execute(program, *args):
     """
@@ -31,6 +38,12 @@ def install_boost(directory, version):
     """
         Installs the right version of Boost for the platform.
     """
+    if not is_windows() and version != ['karmic']:
+        version = version[0]
+        if not os.path.isdir('%s/Boost/1_%s_0' % (directory, version)):
+            sys.symlink('Boost/1_%s_0' % version, '%s/Boost/1_%s_0' % (directory, version))
+        if not os.path.isdir('%s/Boost/boost/include/boost-1_%s' % (directory, version)):
+            execute('%s/Boost/build', *version)
     return execute('%s/Boost/install' % directory, *boost)
 
 
