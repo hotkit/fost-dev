b28c39131807fbb9bf977b7c8b9a2e765c293f2f
Kirit Sælensminde <k@kirit.com>
2010-02-27 12:46:15 +0000

    Gone back to the simpler Boost version configuration and added more platform specific handlers and added svn up for the project as well.

--- test-fost.py
+++ test-fost.py
@@ -15,7 +15,7 @@ PROJECTS = {
     'fost-py': {},
 }
 SUFFIXES = ['', '-rc']
-BOOST_VERSIONS = [[b, 0] for b in range(38, 43)]
+BOOST_VERSIONS = list(range(38, 43))
 if sys.platform == 'linux2':
     BOOST_VERSIONS.append(['karmic'])
 
@@ -26,41 +26,48 @@ def is_windows():
     """
     return sys.platform == 'win32'
 
+
 def execute(program, *args):
     """
         Execute a program and return True if it worked.
     """
     command = '%s %s' % (program, ' '.join([str(a) for a in args]))
-    print program, args, command
+    print "Starting", command
     return os.system(command) == 0
+
 
 def install_boost(directory, version):
     """
         Installs the right version of Boost for the platform.
     """
-    if not is_windows() and version != ['karmic']:
-        v = version[0]
-        if not os.path.isdir('%s/Boost/1_%s_0' % (directory, v)):
-            os.symlink('Boost/1_%s_0' % v, '%s/Boost/1_%s_0' % (directory, v))
-        if not os.path.isdir('%s/Boost/boost/include/boost-1_%s' % (directory, v)):
-            execute('%s/Boost/build', *version)
-    return execute('%s/Boost/install' % directory, *version)
+    if not is_windows() and version != 'karmic':
+        if not os.path.isdir('%s/Boost/1_%s_0' % (directory, version)):
+            os.symlink('Boost/1_%s_0' % v, '%s/Boost/1_%s_0' % (directory, version))
+        if not os.path.isdir('%s/Boost/boost/include/boost-1_%s' % (directory, version)):
+            execute('%s/Boost/build' % directory, version, 0)
+    if is_windows():
+        return execute('%s/Boost/install' % directory, version, 0, '../../Boost')
+    else:
+        return execute('%s/Boost/install' % directory, version, 0)
 
 
 built, success, failure = 0, [], []
 if execute('svn', 'up', '--ignore-externals'):
     for project, configuration in PROJECTS.items():
         for suffix in configuration.get('suffixes', SUFFIXES):
-            for boost in configuration.get('boost', BOOST_VERSIONS):
-                directory = '%s%s' % (project, suffix)
-                if not install_boost(directory, boost):
-                    raise "Boost install failed"
-                else:
-                    built += 1
-                    if not execute('%s/%s/compile' % (directory, project)):
-                        raise "Project failed"
+            directory = '%s%s' % (project, suffix)
+            if execute('svn', 'up', directory):
+                for boost in configuration.get('boost', BOOST_VERSIONS):
+                    if not install_boost(directory, boost):
+                        raise "Boost install failed"
                     else:
-                        success.append([project, suffix, boost])
+                        built += 1
+                        if not execute('%s/%s/compile' % (directory, project)):
+                            raise "Project failed"
+                        else:
+                            success.append([project, suffix, boost])
+            else:
+                raise "svn up failed"
 
 for project, suffix, boost in success:
     print "Success", project + suffix, "Boost", boost
