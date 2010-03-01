1d19de394bcaa826b183c8559c58c0b7a1465906
Kirit Sælensminde <k@kirit.com>
2010-03-01 01:01:28 +0000

    The script now just updates the test builder files and then runs the Python build tester.

--- test-fost.cmd
+++ test-fost.cmd
@@ -1,19 +1,3 @@
 @echo off
-for %%t in (fost-base fost-internet fost-aws fost-orm fost-postgres fost-py fost-meta) DO (
-    for %%b in (37 38 39 40 41 42) DO (
-        call do-build.cmd %%t %%b %%t
-        IF ERRORLEVEL 1 (
-            copy
-            GOTO end
-        ) ELSE (
-            for %%s in (-rc) DO (
-                call do-build.cmd %%t%%s %%b %%t
-                IF ERRORLEVEL 1 (
-                    copy
-                    GOTO end
-                )
-            )
-        )
-    )
-)
-:end
+svn up --ignore-externals
+test-fost.py %*
