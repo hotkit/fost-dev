18dde1761df0a0b612c44f1f6130fb2e394f2890
Kirit Sælensminde <k@kirit.com>
2010-06-23 09:47:25 +0000

    Added proper Boost building bypassing for Lucid and display when we are soft linking.

--- test-fost.py
+++ test-fost.py
@@ -34,9 +34,11 @@ def install_boost(directory, version):
     """
         Installs the right version of Boost for the platform.
     """
-    if not is_windows() and version != 'karmic':
-        if not os.path.isdir('%s/Boost/1_%s_0' % (directory, version)):
-            os.symlink('../../Boost/1_%s_0' % version, '%s/Boost/1_%s_0' % (directory, version))
+    if not is_windows() and not version in ['karmic', 'lucid']:
+        path = '%s/Boost/1_%s_0' % (directory, version)
+        if not os.path.isdir(path):
+            print "Soft-linking to", path
+            os.symlink('../../Boost/1_%s_0' % version, path)
         if not os.path.isdir('%s/Boost/boost/include/boost-1_%s' % (directory, version)):
             execute('%s/Boost/build' % directory, version, 0)
     if is_windows():
