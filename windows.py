86698be25f404c01896c5294fa78c130629e706f
Kirit Sælensminde <k@kirit.com>
2011-01-11 04:44:35 +0000

    Set up the configuration for the fost-windows project.

--- windows.py
+++ windows.py
@@ -3,3 +3,8 @@ from configuration import *
 
 if 37 in BOOST_VERSIONS:
     BOOST_VERSIONS.remove(37)
+
+PROJECTS['fost-windows'] = dict(
+    suffixes = ['', '-stable'],
+    variants = ['debug-mfc', 'release-mfc'],
+)
