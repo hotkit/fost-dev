b5f626dd5429d86cdbaa35cc128a0eff6a044ebc
Kirit Sælensminde <k@kirit.com>
2012-11-12 03:50:10 +0000

    Changed support for Precise and the default Boost version to test against to be the one that ships with Lucid.

--- configuration.py
+++ configuration.py
@@ -3,6 +3,6 @@
 
 PROJECTS = {}
 SUFFIXES = ['', '-rc']
-BOOST_VERSIONS = [41, 47]
+BOOST_VERSIONS = [40, 47]
 VARIANTS = ['debug', 'release']
 TARGETS = ['all', '']
--- precise.py
+++ precise.py
@@ -2,6 +2,5 @@
 from configuration import *
 
 BOOST_VERSIONS.append('precise')
-for v in xrange(35, 47):
-    if v in BOOST_VERSIONS:
-        BOOST_VERSIONS.remove(v)
+if 41 in BOOST_VERSIONS:
+    BOOST_VERSIONS.remove(41)
