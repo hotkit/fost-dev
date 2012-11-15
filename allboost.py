88d55ad995c8808c969931aa270769191fbc4e6b
Kirit Sælensminde <k@kirit.com>
2012-11-15 05:30:36 +0000

    We should test up to Boost 1.52.

--- allboost.py
+++ allboost.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from configuration import *
 
-for v in range(40, 50):
+for v in range(40, 53):
     if v not in BOOST_VERSIONS and (v < 43 or v > 44):
         BOOST_VERSIONS.append(v)
