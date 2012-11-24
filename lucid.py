21dbd7b43966286cf9f90fa43b380642eee5adbd
Kirit Sælensminde <k@kirit.com>
2012-11-24 06:31:01 +0000

    Build against Boost 1.40 unless we're on Lucid as that's the version that ships with Lucid.

--- lucid.py
+++ lucid.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 from configuration import *
 
+# Boost 1.40 is the version that ships with Lucid
+if 40 in BOOST_VERSIONS:
+    BOOST_VERSIONS.remove(40)
+
 BOOST_VERSIONS.append('lucid')
