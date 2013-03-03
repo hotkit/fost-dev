9555067651aa3ad46de9e10d89668e19182e8c19
Kirit Sælensminde <k@kirit.com>
2013-03-03 09:21:31 +0000

    For Quantal the earliest Boost library we can use is 1.49.0.

--- quantal.py
+++ quantal.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from configuration import *
 
+if not 49 in BOOST_VERSIONS:
+    BOOST_VERSIONS.append(49)
 BOOST_VERSIONS.append('quantal')
-for v in xrange(35, 47):
+for v in xrange(35, 49):
     if v in BOOST_VERSIONS:
         BOOST_VERSIONS.remove(v)
