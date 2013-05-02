2c4778e10f71cd11b021987b58a2dc07d42d4db4
Kirit Sælensminde <k@kirit.com>
2013-05-02 05:17:24 +0000

    Added support for Ubuntu raring.

---
+++ raring.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from configuration import *
+
+if not 49 in BOOST_VERSIONS:
+    BOOST_VERSIONS.append(49)
+BOOST_VERSIONS.append('raring')
+for v in xrange(35, 49):
+    if v in BOOST_VERSIONS:
+        BOOST_VERSIONS.remove(v)
