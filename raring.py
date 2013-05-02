c0fad369410571b7da5eb99446920803ee07b088
Kirit Sælensminde <k@kirit.com>
2013-05-02 12:58:46 +0000

    Boost 1.49.0 doesn't build properly under raring.

--- raring.py
+++ raring.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from configuration import *
 
-if not 49 in BOOST_VERSIONS:
-    BOOST_VERSIONS.append(49)
 BOOST_VERSIONS.append('raring')
-for v in xrange(35, 49):
+for v in xrange(35, 50):
     if v in BOOST_VERSIONS:
         BOOST_VERSIONS.remove(v)
