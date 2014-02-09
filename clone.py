2ea3ca6288dcc10031443611b6d3e1ae61832f85
Kirit Sælensminde <k@kirit.com>
2014-02-09 12:41:52 +0700

    Fix a typo.

--- clone.py
+++ clone.py
@@ -2,7 +2,7 @@ from configuration import *
 
 
 for project, configuration in PROJECTS.items():
-    if not os.exists(project):
+    if not os.path.exists(project):
         worked('git', 'clone', configuration['source'], project)
         worked('cd', project, '&&', 'git', 'flow', 'init', '-d')
 
