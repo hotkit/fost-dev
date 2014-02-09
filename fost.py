1dc8e816e15bd1322ba5d9377c36f2d2b5bd1e57
Kirit Sælensminde <k@kirit.com>
2014-02-09 16:37:43 +0700

    Add fost-internet.

--- fost.py
+++ fost.py
@@ -4,6 +4,8 @@ from configuration import *
 
 PROJECTS['fost-base'] = dict(
     source='git@github.com:KayEss/fost-base-dev.git')
+PROJECTS['fost-internet'] = dict(
+    source='git@github.com:KayEss/fost-internet-dev.git')
 
 
 PROJECTS['hello'] = dict(
