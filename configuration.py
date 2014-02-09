8075e662a58390bdf2641cd3c26ba325af7fcc2b
Kirit Sælensminde <k@kirit.com>
2014-02-09 11:31:59 +0700

    Start to reconfigure the dev script to just be an option loader.

--- configuration.py
+++ configuration.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 
 
 PROJECTS = {}
-SUFFIXES = ['', '-rc']
 BOOST_VERSIONS = [40, 47, 52]
 VARIANTS = ['debug', 'release']
 TARGETS = ['all', '']
+
+
+def is_windows():
+    """
+        Returns True if the system is Windows.
+    """
+    return sys.platform == 'win32'
+
+
+def execute(program, *args):
+    """
+        Execute a program and return True if it worked.
+    """
+    if is_windows():
+        program = program.replace('/', '\\')
+    command = '%s %s' % (program, ' '.join([str(a) for a in args]))
+    print "Starting", command
+    return os.system(command) == 0
+
+
+def worked(program, *args):
+    """
+        Execute and make sure the command worked.
+    """
+    assert execute(program, *args)
--- dev.py
+++ dev.py
@@ -1,31 +1,6 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
 import sys
-
-from configuration import *
-
-for arg in sys.argv[1:]:
-    print "Importing", arg
-    __import__(arg)
-
-
-def is_windows():
-    """
-        Returns True if the system is Windows.
-    """
-    return sys.platform == 'win32'
-
-
-def execute(program, *args):
-    """
-        Execute a program and return True if it worked.
-    """
-    if is_windows():
-        program = program.replace('/', '\\')
-    command = '%s %s' % (program, ' '.join([str(a) for a in args]))
-    print "Starting", command
-    return os.system(command) == 0
 
 
 def update(directory):
@@ -59,30 +34,38 @@ def install_boost(directory, version):
         return execute('%s/Boost/install' % directory, version, 0)
 
 
-built, success, failure = 0, [], []
-for project, configuration in PROJECTS.items():
-    for suffix in configuration.get('suffixes', SUFFIXES):
-        directory = '%s%s' % (configuration.get('folder', project), suffix)
-        if update(directory):
-            for boost in configuration.get('boost', BOOST_VERSIONS):
-                if not install_boost(directory, boost):
-                    raise "Boost install failed"
-                else:
-                    for variant in configuration.get('variants', VARIANTS):
-                        for target in configuration.get('targets', TARGETS):
-                            built += 1
-                            if not execute('./compile', directory, project, boost,  variant, target):
-                                failure.append([project, suffix, boost, variant, target])
-                            else:
-                                success.append([project, suffix, boost, variant, target])
-                                break
-        else:
-            raise "svn up failed"
+def do_test():
+    built, success, failure = 0, [], []
+    for project, configuration in PROJECTS.items():
+        for suffix in configuration.get('suffixes', SUFFIXES):
+            directory = '%s%s' % (configuration.get('folder', project), suffix)
+            if update(directory):
+                for boost in configuration.get('boost', BOOST_VERSIONS):
+                    if not install_boost(directory, boost):
+                        raise "Boost install failed"
+                    else:
+                        for variant in configuration.get('variants', VARIANTS):
+                            for target in configuration.get('targets', TARGETS):
+                                built += 1
+                                if not execute('./compile', directory, project, boost,  variant, target):
+                                    failure.append([project, suffix, boost, variant, target])
+                                else:
+                                    success.append([project, suffix, boost, variant, target])
+                                    break
+            else:
+                raise "svn up failed"
 
-def status(k, l):
-    for project, suffix, boost, variant, target in l:
-        print k, project + suffix, "Boost", boost, \
-            "Variant:", variant, "Target:", target
-status("Success", success)
-status("Failure", failure)
-print "Total built", built, "Total success", len(success)
+    def status(k, l):
+        for project, suffix, boost, variant, target in l:
+            print k, project + suffix, "Boost", boost, \
+                "Variant:", variant, "Target:", target
+    status("Success", success)
+    status("Failure", failure)
+    print "Total built", built, "Total success", len(success)
+
+
+if __name__ == "__main__":
+    from configuration import *
+    for arg in sys.argv[1:]:
+        print "Importing", arg
+        __import__(arg)
