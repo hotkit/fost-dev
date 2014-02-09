71f3e5bedff2dcb76c9aa66b1522e9e0c76c6ca7
Kirit Sælensminde <k@kirit.com>
2014-02-09 16:11:55 +0700

    Use the projects generator so we get the correct folder names.

--- push.py
+++ push.py
@@ -2,11 +2,11 @@ from configuration import *
 
 
 git('.', 'push')
-for project, configuration in PROJECTS.items():
-    git(project, 'push')
-    git(project, 'push', '--tags')
+for project, folder, configuration in projects():
+    git(folder, 'push')
+    git(folder, 'push', '--tags')
     for lib in configuration['libs']:
-        folder = os.path.join(project, lib)
-        git(folder, 'push')
-        git(folder, 'push', '--tags')
+        subfolder = os.path.join(project, lib)
+        git(subfolder, 'push')
+        git(subfolder, 'push', '--tags')
 
