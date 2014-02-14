db073f0bb8f6f5a1f0fd800812aed4cc2e03e87f
Kirit Sælensminde <k@kirit.com>
2014-02-14 17:49:54 +0700

    Add the fost-meta project in.

--- fost.py
+++ fost.py
@@ -2,14 +2,16 @@
 from configuration import *
 
 
-for lib in ['fost-aws', 'fost-base', 'fost-internet', 'fost-orm', 'fost-postgres',
-        'fost-py', 'fost-windows']:
+for lib in ['fost-aws', 'fost-base', 'fost-internet', 'fost-orm',
+        'fost-postgres', 'fost-py', 'fost-windows']:
     PROJECTS[lib] = dict(
         source='git@github.com:KayEss/%s-dev.git' % lib)
 
+PROJECTS['fost-meta'] = dict(
+    source='git@github.com:KayEss/fost-meta.git',
+    libs=[])
 PROJECTS['fost-postgres']['post-clone'] = ['PostgreSQL/configure']
 PROJECTS['fost-windows']['test'] = False
-
 PROJECTS['hello'] = dict(
     source='git@github.com:KayEss/fost-hello.git',
     folder='fost-hello',
