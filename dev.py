67f6095d166ca4bc47bdae961bef52db1a24bc45
Kirit Sælensminde <k@kirit.com>
2014-02-23 12:46:23 +0700

    Removed some dead code.

--- dev.py
+++ dev.py
@@ -4,17 +4,6 @@ import subprocess
 import sys
 
 
-def update(directory):
-    """
-        Update the code to the latest version.
-    """
-    updater = os.path.join(directory, 'update' if not is_windows() else 'update.cmd')
-    if os.path.exists(updater):
-        return execute(updater)
-    else:
-        return execute('svn', 'up', directory)
-
-
 if __name__ == "__main__":
     from configuration import *
     for arg in sys.argv[1:]:
