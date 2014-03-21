d6fd84f4dd93f6f6c51ce09f4d99637069fd0812
Kirit Sælensminde <k@kirit.com>
2014-03-21 17:32:18 +0700

    Add AnimRay to the list of projects

--- allprojects.py
+++ allprojects.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
+from animray import *
 from fost import *
---
+++ animray.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from configuration import *
+
+
+PROJECTS['animray'] = dict(
+    source='git@github.com:KayEss/AnimRay.git',
+    folder='AnimRay',
+    libs=[],
+    targets=['toolset=clang'])
+
