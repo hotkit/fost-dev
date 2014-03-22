f5ed3389c65e801f14a88ac62fbf546cce475e91
Kirit Sælensminde <k@kirit.com>
2014-03-22 19:33:53 +0700

    Added beanbag.

---
+++ beanbag.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from configuration import *
+
+
+PROJECTS['fost-beanbag'] = {
+    'source': 'git@github.com:KayEss/beanbag.git',
+    'folder': 'beanbag',
+    'libs': ['fost-beanbag'],
+}
+
