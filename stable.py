0e99a4161cba80752944578d8d5c429fd452128f
Kirit Sælensminde <k@kirit.com>
2014-06-07 16:58:27 +0700

    We don't need to push tags.

--- stable.py
+++ stable.py
@@ -11,11 +11,9 @@ def stable():
                     git(location, 'merge', '--ff-only', 'remotes/origin/%s' % branch)
                 git(location, 'merge', '--no-ff', 'develop')
                 git(location, 'push')
-                git(location, 'push', '--tags')
                 for lib in configuration['libs']:
                     subfolder = os.path.join(folder, lib)
                     git(subfolder, 'push')
-                    git(subfolder, 'push', '--tags')
                 git(location, 'checkout', 'develop')
 
 
