34488f6c64e7b9a7fdb4bdc33d6dab603bbdff8b
Kirit Sælensminde <k@kirit.com>
2014-08-15 14:26:51 +0700

    Fix project name.

--- mengmom.py
+++ mengmom.py
@@ -2,7 +2,7 @@
 from configuration import *
 
 
-PROJECTS['animray'] = dict(
+PROJECTS['mengmom'] = dict(
     source='git@github.com:KayEss/mengmom.git',
     folder='mengmom',
     libs=['fost-web'])
