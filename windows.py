37f929068bbbec6d0b9222479a238391a1dbc0b2
Kirit Saelensminde <k@kirit.com>
2014-09-03 09:23:12 +0700

    Only try to configure OpenSSL on Windows for those projects that are going to be built as C++ projects.

--- windows.py
+++ windows.py
@@ -9,9 +9,10 @@ if 37 in BOOST_VERSIONS:
     BOOST_VERSIONS.remove(37)
 
 for project, configuration in PROJECTS.items():
-    if configuration.has_key('post-clone'):
-        configuration['post-clone'].append('&&')
-        configuration['post-clone'].append('OpenSSL\\configure')
-    else:
-        configuration['post-clone'] = ['OpenSSL\\configure']
+    if len(configuration.get('targets', ['all', ''])):
+        if configuration.has_key('post-clone'):
+            configuration['post-clone'].append('&&')
+            configuration['post-clone'].append('OpenSSL\\configure')
+        else:
+            configuration['post-clone'] = ['OpenSSL\\configure']
 
