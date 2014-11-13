6ade92984ebebfebf5c5e7f9f6b835f7c0ca5144
Kirit Sælensminde <k@kirit.com>
2014-11-13 19:57:40 +0700

    Make sure we recurse down an extra level when updating submodules.

--- latest.py
+++ latest.py
@@ -2,25 +2,29 @@ from configuration import *
 
 
 def latest():
-    def get_latest(gitflow, folder):
-        if gitflow:
-            git(folder, 'checkout', 'develop')
-        else:
-            git(folder, 'checkout', 'master')
-        git(folder, 'pull')
-        git(folder, 'submodule', 'foreach',
-            "\"(git branch -a | grep '. develop$' > /dev/null 2>&1)"
-                " && git checkout develop || git checkout master\"")
-        git(folder, 'submodule', 'foreach', "\"git pull\"")
+    def submodules(folder):
         submods = git_capture(folder, 'submodule').split('\n')
         for submod in submods:
             if len(submod) and submod[0] == "+":
                 name = submod.split(' ')[1]
                 git(folder, 'add', name)
                 git(folder, 'commit', '-m', '"$(git submodule summary %s)"' % name)
-    get_latest(False, ".")
+    def get_latest(gitflow, folder, libs):
+        if gitflow:
+            git(folder, 'checkout', 'develop')
+        else:
+            git(folder, 'checkout', 'master')
+        git(folder, 'pull')
+        git(folder, 'submodule', 'foreach', '--recursive',
+            "\"(git branch -a | grep '. develop$' > /dev/null 2>&1)"
+                " && git checkout develop || git checkout master\"")
+        git(folder, 'submodule', 'foreach', "\"git pull\"")
+        submodules(folder)
+        for lib in libs:
+            submodules(os.path.join(folder, lib))
+    get_latest(False, ".", [])
     for project, folder, configuration in projects():
-        get_latest(configuration.get('gitflow', True), folder)
+        get_latest(configuration.get('gitflow', True), folder, configuration.get('libs', []))
 
 
 ACTIONS.append(latest)
