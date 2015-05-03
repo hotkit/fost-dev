cc321a46a58cc69f19306eac1806ab7d390ade42
Kirit Sælensminde <k@kirit.com>
2015-05-03 13:18:03 +0700

    Separate fost-internet so it can be build separately.

--- fost.py
+++ fost.py
@@ -2,12 +2,12 @@
 from configuration import *
 import beanbag
 import mengmom
-for lib in ['fost-android', 'fost-aws', 'fost-hello', 'fost-meta',
-        'fost-postgres', 'fost-py']:
+for lib in ['fost-android', 'fost-aws', 'fost-hello', 'fost-internet',
+        'fost-meta', 'fost-postgres', 'fost-py']:
     __import__(lib)
 
 
-for lib in ['fost-base', 'fost-internet', 'fost-orm', 'fost-windows']:
+for lib in ['fost-base', 'fost-orm', 'fost-windows']:
     PROJECTS[lib] = dict(
         source='git@github.com:KayEss/%s-dev.git' % lib,
         libs=[lib])
