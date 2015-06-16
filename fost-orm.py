c277705534887a651baddfc18f63b9b832d912e9
Kirit Sælensminde <k@kirit.com>
2015-06-16 09:25:09 +0700

    Split fost-orm out to its own file.

---
+++ fost-orm.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from configuration import *
+
+
+PROJECTS['fost-orm'] = dict(
+    source='git@github.com:KayEss/fost-orm-dev.git',
+    libs=['fost-orm'])
+
--- fost.py
+++ fost.py
@@ -3,14 +3,8 @@ from configuration import *
 import beanbag
 import mengmom
 for lib in ['fost-android', 'fost-aws', 'fost-base', 'fost-hello',
-        'fost-internet', 'fost-meta', 'fost-postgres', 'fost-py']:
+        'fost-internet', 'fost-orm', 'fost-meta', 'fost-postgres', 'fost-py']:
     __import__(lib)
-
-
-for lib in ['fost-orm']: #, 'fost-windows']:
-    PROJECTS[lib] = dict(
-        source='git@github.com:KayEss/%s-dev.git' % lib,
-        libs=[lib])
 
 #PROJECTS['fost-windows']['test'] = False
 
