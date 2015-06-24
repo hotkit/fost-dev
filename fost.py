94f97212d1e9b9df123e94162bf8678857f1f03e
Kirit Sælensminde <k@kirit.com>
2015-06-24 20:58:00 +0700

    Remove fost-windows for now.

--- fost.py
+++ fost.py
@@ -2,9 +2,7 @@
 from configuration import *
 import beanbag
 import mengmom
-for lib in ['fost-android', 'fost-aws', 'fost-base', 'fost-hello',
-        'fost-internet', 'fost-orm', 'fost-meta', 'fost-postgres', 'fost-py']:
+for lib in ['f5-threading', 'fost-android', 'fost-aws', 'fost-base',
+        'fost-hello', 'fost-internet', 'fost-orm', 'fost-meta',
+        'fost-postgres', 'fost-py']:
     __import__(lib)
-
-#PROJECTS['fost-windows']['test'] = False
-
