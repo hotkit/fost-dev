49f38a4b717e23f016072c36b5eba6993b9899e0
Kirit Sælensminde <k@kirit.com>
2016-09-10 09:46:58 +0700

    Configure Postgres when building fost-meta

--- fost-meta.py
+++ fost-meta.py
@@ -6,3 +6,9 @@ PROJECTS['fost-meta'] = dict(
     source='git@github.com:KayEss/fost-meta.git',
     libs=[])
 
+
+if is_windows():
+    PROJECTS['fost-meta']['post-clone'] = ['PostgreSQL\\configure']
+else:
+    PROJECTS['fost-meta']['post-clone'] = ['PostgreSQL/configure']
+
