5269f8e20ebecd18445a1fb2af23ac4e9f61ad8c
Kirit Sælensminde <k@kirit.com>
2016-11-11 16:22:17 +0700

    Add wright.

---
+++ wright.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from configuration import *
+
+
+PROJECTS['wright'] = {
+    'source': 'git@github.com:KayEss/wright.git',
+    'gitflow': False, 'test': False,
+    'libs': []}
