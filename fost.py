111cd4567a4743deeff1dab2cc22b83e1fc7fab5
Kirit Sælensminde <k@kirit.com>
2017-12-19 13:35:18 +0700

    Remove fost-android as it's a special project

--- fost.py
+++ fost.py
@@ -4,7 +4,7 @@ import f5
 import beanbag
 import mengmom
 import wright
-for lib in ['fost-android', 'fost-aws', 'fost-base',
+for lib in ['fost-aws', 'fost-base',
         'fost-hello', 'fost-internet', 'fost-orm', 'fost-meta',
         'fost-postgres', 'fost-py']:
     __import__(lib)
