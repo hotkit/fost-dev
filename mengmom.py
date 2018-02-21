1b71402da76b22c3a7d5cdcaf7814f3bb8e163e7
Kirit Sælensminde <k@kirit.com>
2018-02-21 14:22:33 +0700

    Odin is also a sub-library of the Mengmom project

--- mengmom.py
+++ mengmom.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 from configuration import *
 
 
 PROJECTS['mengmom'] = dict(
     source='git@github.com:KayEss/mengmom.git',
-    libs=['fostgres', 'fost-web'])
+    libs=['fostgres', 'fost-web', 'odin'])
 
 if is_windows():
     PROJECTS['mengmom']['post-clone'] = ['PostgreSQL\\configure']
