44a32938d3f5ea99eb6886c3e492b0b08e0e1050
Kirit Sælensminde <kirit@kirit.com>
2018-05-08 13:52:00 +0700

    Add test-android

---
+++ test-android.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from configuration import *
+
+
+PROJECTS['test-android'] = dict(
+    source='git@github.com:KayEss/test-android.git',
+    libs=['fost-android-ndk'],
+    targets=[],
+    test=False)
+
