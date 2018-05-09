c224eb384ed2fd0d07d79f4917dcf2c138fc53ec
Kirit Sælensminde <kirit@kirit.com>
2018-05-09 13:37:25 +0700

    Switch boost versions.

--- cmake.py
+++ cmake.py
@@ -12,14 +12,23 @@ def dotests():
             continue
         elif runtests == True:
             for toolset in ['gcc', 'clang']:
-                for variant in ["Release", "Debug"]:
-                    built += 1
-                    buildpath = '/'.join([project, 'build.tmp', toolset, variant])
-                    mkpath(buildpath)
-                    cmd1 = ([] if toolset == 'gcc' else ['CC=clang', 'CXX=clang++']) + ['cmake', '../../..', '-G', 'Ninja']
-                    worked(*['cd', buildpath, '&&'] + cmd1)
-                    cmd2 = ['ninja', 'check']
-                    worked(*['cd', buildpath, '&&'] + cmd2)
+                for bmajor, bminor, bpatch in [(1, 65, 0), (1, 66, 0)]:
+                    bver = "%d.%d.%d" % (bmajor, bminor, bpatch)
+                    for variant in ["Release", "Debug"]:
+                        built += 1
+                        tname = toolset + '-' + bver + '-'+ variant
+                        buildpath = '/'.join([project, 'build.tmp', tname])
+                        mkpath(buildpath)
+                        cmd1 = ([] if toolset == 'gcc' else ['CC=clang', 'CXX=clang++']) + ['cmake', '../..', '-G', 'Ninja']
+                        conf = lambda n, v: cmd1 + ['-D' + n + '=' + v]
+                        cmd1 = conf('CMAKE_BUILD_TYPE', variant)
+                        cmd1 = conf('BOOST_VMAJOR', str(bmajor))
+                        cmd1 = conf('BOOST_VMINOR', str(bminor))
+                        cmd1 = conf('BOOST_VPATCH', str(bpatch))
+                        cmd1 = conf('CMAKE_INSTALL_PREFIX', '../../dist-' + tname)
+                        worked(*['cd', buildpath, '&&'] + cmd1)
+                        worked('cd', buildpath, '&&', 'ninja', 'check')
+                        worked('cd', buildpath, '&&', 'ninja', 'install')
         else:
             assert execute('cd', project, '&&', runtests)
 
