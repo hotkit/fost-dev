0f513c4a189eb663a4aa521b7b3fc46c4c910a8a
Kirit Sælensminde <kirit@kirit.com>
2018-07-02 13:04:29 +0700

    We only need to set up the cmake build once.

--- test.py
+++ test.py
@@ -46,19 +46,19 @@ def dotests():
                     for variant in VARIANTS:
                         failed = False
                         targets = configuration.get('make', MAKE)
+                        tname = toolset + '-' + bver + '-'+ variant
+                        buildpath = '/'.join([directory, 'build.tmp', tname])
+                        mkpath(buildpath)
+                        cmd1 = ([] if toolset == 'gcc' else ['CC=clang', 'CXX=clang++']) + ['cmake', '../..', '-G', 'Ninja']
+                        conf = lambda n, v: cmd1 + ['-D' + n + '=' + v]
+                        cmd1 = conf('CMAKE_BUILD_TYPE', variant.title())
+                        cmd1 = conf('BOOST_VMAJOR', str(bmajor))
+                        cmd1 = conf('BOOST_VMINOR', str(bminor))
+                        cmd1 = conf('BOOST_VPATCH', str(bpatch))
+                        cmd1 = conf('CMAKE_INSTALL_PREFIX', '../../dist-test/' + tname)
+                        worked(*['cd', buildpath, '&&'] + cmd1)
                         for target in targets:
                             built += 1
-                            tname = toolset + '-' + bver + '-'+ variant
-                            buildpath = '/'.join([directory, 'build.tmp', tname])
-                            mkpath(buildpath)
-                            cmd1 = ([] if toolset == 'gcc' else ['CC=clang', 'CXX=clang++']) + ['cmake', '../..', '-G', 'Ninja']
-                            conf = lambda n, v: cmd1 + ['-D' + n + '=' + v]
-                            cmd1 = conf('CMAKE_BUILD_TYPE', variant.title())
-                            cmd1 = conf('BOOST_VMAJOR', str(bmajor))
-                            cmd1 = conf('BOOST_VMINOR', str(bminor))
-                            cmd1 = conf('BOOST_VPATCH', str(bpatch))
-                            cmd1 = conf('CMAKE_INSTALL_PREFIX', '../../dist-test/' + tname)
-                            worked(*['cd', buildpath, '&&'] + cmd1)
                             if not execute('cd', buildpath, '&&', 'ninja', target):
                                 failure.append([project, (bmajor, bminor, bpatch), variant, [target], toolset])
                                 failed = True
