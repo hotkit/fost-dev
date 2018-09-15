a32ed73e238c67d43691460e0539cac7db029740
Kirit Sælensminde <kirit@kirit.com>
2018-09-15 19:49:11 +0700

    Add json-schema

--- fost-meta.py
+++ fost-meta.py
@@ -4,7 +4,8 @@ from configuration import *
 PROJECTS['fost-meta'] = dict(
         source='git@github.com:KayEss/fost-meta.git',
         libs=['cord', 'fost-aws', 'fost-base', 'fostgres',
-              'fost-internet', 'fost-postgres', 'fost-web', 'odin', 'threading'],
+              'fost-internet', 'fost-postgres', 'fost-web', 'json-schema',
+              'odin', 'threading'],
     )
 
 
