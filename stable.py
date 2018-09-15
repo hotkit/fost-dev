2fe70288065a324dd89ae4d0306b68f2383bbb36
Kirit Sælensminde <kirit@kirit.com>
2018-09-15 19:50:45 +0700

    Stable now only deals with applications.

--- stable.py
+++ stable.py
@@ -4,17 +4,15 @@ from configuration import *
 def stable():
     for project, folder, configuration in projects():
         if configuration.get('gitflow', True):
-            libfolders = [os.path.join(folder, lib) for lib in configuration.get('libs', [])]
-            for location in [folder] + libfolders:
-                git(location, 'fetch', 'origin')
-                for branch in ['develop', 'master']:
-                    git(location, 'checkout', branch)
-                    git(location, 'merge', '--ff-only', 'remotes/origin/%s' % branch)
-                    git(location, 'submodule', 'update', '--init', '--recursive')
-                git(location, 'merge', '--no-ff', 'develop', '-m',
-                    '"Merge from develop\n\n$(git diff develop --stat)"')
-                git(location, 'push')
-                git(location, 'checkout', 'develop')
+            git(folder, 'fetch', 'origin')
+            for branch in ['develop', 'master']:
+                git(folder, 'checkout', branch)
+                git(folder, 'merge', '--ff-only', 'remotes/origin/%s' % branch)
+                git(folder, 'submodule', 'update', '--init', '--recursive')
+            git(folder, 'merge', '--no-ff', 'develop', '-m',
+                '"Merge from develop\n\n$(git diff develop --stat)"')
+            git(folder, 'push')
+            git(folder, 'checkout', 'develop')
 
 
 ACTIONS.append(stable)
