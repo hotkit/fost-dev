225bd5c976701408160b7a9862a2da5c5b19280b
Kirit Sælensminde <kirit@kirit.com>
2018-12-08 19:12:27 +0700

    Include `-Wextra` as well

--- werror.py
+++ werror.py
@@ -1,4 +1,4 @@
 from configuration import MODES
 
-MODES['clang']['werror'] = (['CXXFLAGS="-Wall -Wpedantic -Werror"'], [])
-MODES['gcc']['werror'] = (['CXXFLAGS="-Wall -Wpedantic -Werror"'], [])
+MODES['clang']['werror'] = (['CXXFLAGS="-Wall -Wextra -Wpedantic -Werror"'], [])
+MODES['gcc']['werror'] = (['CXXFLAGS="-Wall -Wextra -Wpedantic -Werror"'], [])
