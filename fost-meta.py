20b2183ac8a2c6181eee43345bef81e54b05505c
Kirit Sælensminde <kirit@kirit.com>
2019-06-22 11:58:28 +0700

    Don't skip fost-beanbag.

--- fost-meta.py
+++ fost-meta.py
@@ -3,7 +3,7 @@ from configuration import *
 
 PROJECTS['fost-meta'] = dict(
         source='git@github.com:KayEss/fost-meta.git',
-        libs=['cord', 'fost-aws', 'fost-base', 'fostgres',
+        libs=['cord', 'fost-aws', 'fost-base', 'fostgres', 'fost-beanbag',
               'fost-internet', 'fost-postgres', 'fost-web', 'json-schema',
               'odin', 'threading'],
         make=['', 'check', 'stress', 'install'],
