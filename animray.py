ac16bf6434b0cfa9b162093b7cab11a0cfbda3bf
Kirit Sælensminde <kirit@kirit.com>
2019-09-22 14:47:00 +0700

    No longer uses Git flow.

--- animray.py
+++ animray.py
@@ -4,8 +4,8 @@ from configuration import *
 
 PROJECTS['animray'] = dict(
     source='git@github.com:KayEss/AnimRay.git',
+    gitflow=False,
     folder='AnimRay',
     libs=[],
     make=['', 'check', 'install'],
 )
-
