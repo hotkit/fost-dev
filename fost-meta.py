0e38bbc7b28143d5eed3d089838cb2a567c3a35b
Kirit Sælensminde <kirit@kirit.com>
2020-03-22 16:50:11 +0700

    Include the makham library.

--- fost-meta.py
+++ fost-meta.py
@@ -5,7 +5,7 @@ PROJECTS['fost-meta'] = dict(
         source='git@github.com:hotkit/fost-meta.git',
         libs=['cord', 'fost-aws', 'fost-base', 'fostgres', 'fost-beanbag',
               'fost-internet', 'fost-postgres', 'fost-web', 'json-schema',
-              'odin', 'threading'],
+              'odin', 'makham', 'threading'],
         make=['', 'check', 'stress', 'install'],
     )
 
