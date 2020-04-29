200c96891db8645837ac06a91b93c9216221def2
Kirit Sælensminde <kirit@kirit.com>
2020-04-29 14:02:51 +0700

    We can build the coroutines with focal.

--- focal.py
+++ focal.py
@@ -1,9 +1,10 @@
-from configuration import BOOST, platform, TOOLSET
+from configuration import BOOST, MODES, platform, TOOLSET
 
 
 platform('focal')
 TOOLSET['gcc']['env'] = ['''CC=gcc-10''', '''CXX=g++-10''']
 TOOLSET['gcc']['cmake'] = ['''-DBOOST_TOOLSET=gcc-10''']
+MODES['gcc'][''].cmake += ['''-DUSE_TAMARIND_LIBRARY=ON''']
 try:
     BOOST.remove((1,70,0))
 except ValueError:
